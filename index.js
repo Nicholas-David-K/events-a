8d9343efc27d256fac03263a7d29548b1bb90fd9
Nicholas David <onedavid.nicholas@gmail.com>
2022-03-29 14:43:00 +0300

    origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(routesHandler);
 
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
